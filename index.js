d304e7b1ae16b16cbb6dd90c84213e19ad23cd9f
Mike <Michaelfray1@gmail.com>
2023-11-30 14:25:17 +0100

    backendGUESTBOOK

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); // Update the path to the static files
 
+// Läs befintlig data från users.json om filen finns
 let users = [];
-
 try {
-  const data = fs.readFileSync('users.json', 'utf8');
+  const data = fs.readFileSync("users.json", "utf8");
   if (data) {
     users = JSON.parse(data);
   }
 } catch (err) {
-  console.error('Fel vid läsning av filen:', err);
+  console.error("Fel vid läsning av filen:", err);
 }
 
-const dirname = __dirname;
-
-app.get('/', (req, res) => {
-  res.sendFile(dirname + '/guestbook.html');
+// Route för att skicka HTML-formuläret till klienten
+app.get("/", (req, res) => {
+  let output = "";
+  if (users && users.length > 0) {
+    for (let i = 0; i < users.length; i++) {
+      output += `<p><b>
+      Name: ${users[i].Name}<br>  
+      Email: ${users[i].Email}<br>
+      Tel: ${users[i].Tel}<br>
+      Lärosäte: ${users[i].Lärosäte}<br>
+      Comment: ${users[i].Comment}</p>`;
+    }
+  }
+  let html = fs.readFileSync(__dirname + "/guestbook.html").toString();
+  html = html.replace("GÄSTER", output);
+  res.send(html);
 });
 
-app.post('/submit', (req, res) => {
-  const { name, email, phonenumber, homepage, message } = req.body;
-  users.push({ name, email, phonenumber, homepage, message });
+// Route för att hantera POST-förfrågningar från formuläret
+app.post("/submit", (req, res) => {
+  const { Name, Email, Lärosäte, Tel, Comment } = req.body;
+  users.push({ Name, Email, Lärosäte, Tel, Comment });
 
-  fs.writeFile('users.json', JSON.stringify(users), (err) => {
+  // Skriv användarinformationen till users.json-filen
+  fs.writeFile("users.json", JSON.stringify(users), (err) => {
     if (err) {
-      console.error('Fel vid skrivning till filen:', err);
-      return res.status(500).send('Serverfel');
+      console.error("Fel vid skrivning till filen:", err);
+      return res.status(500).send("Serverfel");
     }
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
-app.get('/users', (req, res) => {
+// Route för att visa alla användare
+app.get("/users", (req, res) => {
   res.json(users);
 });
 
+// Starta servern på port 3000
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
